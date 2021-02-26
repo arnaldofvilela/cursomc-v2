44e8e783d84971dc0d8d4f5760f2c63ffe6cdfd6
Arnaldo Neto <vilela-neto@hotmail.com>
2021-02-26 19:59:44 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/arnaldoneto/cursomc/services/ClienteService.java
+++ src/main/java/com/arnaldoneto/cursomc/services/ClienteService.java
@@ -5,19 +5,19 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.arnaldoneto.cursomc.domain.Categoria;
-import com.arnaldoneto.cursomc.repositories.CategoriaRepository;
+import com.arnaldoneto.cursomc.domain.Cliente;
+import com.arnaldoneto.cursomc.repositories.ClienteRepository;
 import com.arnaldoneto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class CategoriaService {
+public class ClienteService {
 
 	@Autowired
-	private CategoriaRepository repo;
+	private ClienteRepository repo;
 	
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
 }
